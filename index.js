969e16b9261df15662b6c069512f964a2eae8b28
Clemens Kolbitsch <clemens.kolbitsch90@gmail.com>
2025-06-09 09:42:43 +0200

    Userstory2
    changed default page to no data
    
    added dropdown menu to choose different stations and implemented in javascript

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-function renderStation54(timestamps, temperatures) {
-    const detailsList = document.getElementById("station54");
+// set station Id to select of menu + loads station with select
+// leaves the selected station in the menu
+
+window.addEventListener("DOMContentLoaded", () => {
+    const dropdownButton = document.getElementById("dropdownMenuButton");
+
+    document.getElementById("leibnitz-wagna").addEventListener("click", (e) => {
+        e.preventDefault();
+        dropdownButton.textContent = "Leibnitz-Wagna"; // update label
+        loadStationData(54);
+    });
+
+    document.getElementById("graz-strassgang").addEventListener("click", (e) => {
+        e.preventDefault();
+        dropdownButton.textContent = "Graz-Straßgang"; // update label
+        loadStationData(16413);
+    });
+});
+
+
+function renderStationData(timestamps, temperatures) {
+    const detailsList = document.getElementById("stationData");
     detailsList.innerHTML = "";
 // checks if timestamps and temperatures are valid arrays
     if (!Array.isArray(timestamps) || !Array.isArray(temperatures)) {
@@ -25,10 +45,11 @@ function renderStation54(timestamps, temperatures) {
     });
 }
 
-async function loadStation54() {
+async function loadStationData(id) {
+    if (!id) return; // no station selected
     try {
-        const response = await fetch(
-            "https://dataset.api.hub.geosphere.at/v1/station/historical/klima-v2-1h?station_ids=54&parameters=tl&start=2024-01-01T00:00:00Z&end=2025-01-01T23:59:59Z"
+        const response = await fetch(`
+                    https://dataset.api.hub.geosphere.at/v1/station/historical/klima-v2-1h?station_ids=${id}&parameters=tl&start=2024-01-01T00:00:00Z&end=2025-01-01T23:59:59Z`
         );
         const data = await response.json();
 
@@ -59,10 +80,9 @@ async function loadStation54() {
             }
         });
 
-        renderStation54(midday, middayTemperatures);
+        renderStationData(midday, middayTemperatures);
     } catch (error) {
         console.error("Error loading details:", error);
     }
 }
 
-window.onload = loadStation54;
